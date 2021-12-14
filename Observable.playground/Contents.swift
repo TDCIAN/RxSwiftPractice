fc3d35e6b2825142baf751d6202a758dabf95d7d
TDCIAN <tdcian71@gmail.com>
2021-12-15 08:16:56 +0900

    error handling

--- Observable.playground/Contents.swift
+++ Observable.playground/Contents.swift
@@ -1,3 +1,126 @@
-import UIKit
+import Foundation
+import RxSwift
 
-var greeting = "Hello, playground"
+print("------ just ------")
+Observable<Int>.just(1)
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("------ of 1 ------")
+Observable<Int>.of(1, 2, 3, 4, 5)
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("------ of 2 ------")
+Observable.of([1, 2, 3, 4, 5])
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("------ From ------")
+Observable.from([1, 2, 3, 4, 5])
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("------ subscribe1 ------")
+Observable.of(1, 2, 3)
+    .subscribe {
+        print($0)
+    }
+
+print("------ subscribe2 ------")
+Observable.of(1, 2, 3)
+    .subscribe {
+        if let element = $0.element {
+            print(element)
+        }
+    }
+
+print("------ subscribe3 ------")
+Observable.of(1, 2, 3)
+    .subscribe(onNext: {
+        print($0)
+    })
+
+print("------ empty ------")
+Observable<Void>.empty()
+    .subscribe {
+        print($0)
+    }
+
+print("------ never ------")
+Observable<Void>.never()
+    .subscribe(
+        onNext: {
+            print($0)
+        },
+        onCompleted: {
+            print("Completed")
+        }
+    )
+
+print("------ range ------")
+Observable.range(start: 1, count: 9)
+    .subscribe(onNext: {
+        print("2*\($0)=\(2*$0)")
+    })
+
+print("------ dispose ------")
+Observable.of(1, 2, 3)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .dispose()
+
+print("------ dispose bag ------")
+let disposeBag = DisposeBag()
+Observable.of(1, 2, 3)
+    .subscribe {
+        print($0)
+    }
+    .disposed(by: disposeBag)
+
+print("------ create 1 ------")
+Observable.create { observer -> Disposable in
+    observer.onNext(1)
+//    observer.on(.next(1))
+    observer.onCompleted()
+//    observer.on(.completed)
+    observer.onNext(2)
+    return Disposables.create()
+}
+.subscribe {
+    print($0)
+}
+.disposed(by: disposeBag)
+
+print("------ create 2 ------")
+enum MyError: Error {
+    case anError
+}
+
+Observable<Int>.create { observer -> Disposable in
+    observer.onNext(1)
+    observer.onError(MyError.anError)
+    observer.onCompleted()
+    observer.onNext(2)
+    return Disposables.create()
+}
+.subscribe(
+    onNext: {
+        print($0)
+    },
+    onError: {
+        print($0.localizedDescription)
+    },
+    onCompleted: {
+        print("completed")
+    },
+    onDisposed: {
+        print("disposed")
+    }
+)
+.disposed(by: disposeBag)
