eba395655de04ed2ac77d67a4d75d1f8a375ec91
TDCIAN <tdcian71@gmail.com>
2021-12-16 07:48:57 +0900

    single, maybe, completable

---
+++ Traits.playground/Contents.swift
@@ -0,0 +1,164 @@
+import UIKit
+import RxSwift
+
+let disposeBag = DisposeBag()
+
+enum TraitsError: Error {
+    case single
+    case maybe
+    case completable
+}
+
+print("---- single 1 -----")
+Single<String>.just("👍")
+    .subscribe(
+        onSuccess: {
+            print($0)
+        },
+        onFailure: {
+            print("Error:\($0)")
+        },
+        onDisposed: {
+            print("disposed")
+        }
+    )
+
+print("---- single 2 -----")
+Observable<String>
+    .create { observer -> Disposable in
+        observer.onError(TraitsError.single)
+        return Disposables.create()
+    }
+    .asSingle()
+    .subscribe(
+        onSuccess: {
+            print($0)
+        },
+        onFailure: {
+            print("Error:\($0)")
+        },
+        onDisposed: {
+            print("disposed")
+        }
+    )
+    .disposed(by: disposeBag)
+
+print("---- single 3 -----")
+struct SomeJSON: Decodable {
+    let name: String
+}
+
+enum JSONError: Error {
+    case decodingError
+}
+
+let json1 = """
+    {"name":"park"}
+    """
+
+let json2 = """
+    {"my_name":"young"}
+    """
+
+func decode(json: String) -> Single<SomeJSON> {
+    Single<SomeJSON>.create { observer -> Disposable in
+        guard let data = json.data(using: .utf8), let json = try? JSONDecoder().decode(SomeJSON.self, from: data) else {
+            observer(.failure(JSONError.decodingError))
+            return Disposables.create()
+        }
+        
+        observer(.success(json))
+        return Disposables.create()
+    }
+}
+
+decode(json: json1)
+    .subscribe {
+        switch $0 {
+        case .success(let json):
+            print(json.name)
+        case .failure(let error):
+            print("error!: \(error.localizedDescription)")
+        }
+    }
+    .disposed(by: disposeBag)
+
+decode(json: json2)
+    .subscribe {
+        switch $0 {
+        case .success(let json):
+            print(json.name)
+        case .failure(let error):
+            print(error)
+        }
+    }
+    .disposed(by: disposeBag)
+
+print("---- maybe 1 -----")
+Maybe<String>.just("👏")
+    .subscribe(
+        onSuccess: {
+            print($0)
+        },
+        onError: {
+            print($0)
+        },
+        onCompleted: {
+            print("completed")
+        },
+        onDisposed: {
+            print("disposed")
+        }
+    )
+    .disposed(by: disposeBag)
+
+print("---- maybe 2 -----")
+Observable<String>.create { observer -> Disposable in
+    observer.onError(TraitsError.maybe)
+    return Disposables.create()
+}
+.asMaybe()
+.subscribe(
+    onSuccess: {
+        print("성공: \($0)")
+    },
+    onError: {
+        print("에러: \($0)")
+    },
+    onCompleted: {
+        print("completed")
+    },
+    onDisposed: {
+        print("disposed")
+    }
+)
+.disposed(by: disposeBag)
+
+
+print("---- completable 1 -----")
+Completable.create { observer -> Disposable in
+    observer(.error(TraitsError.completable))
+    return Disposables.create()
+}
+.subscribe(
+    onCompleted: {
+        print("completed")
+    },
+    onError: {
+        print("error: \($0)")
+    }
+    , onDisposed: {
+        print("disposed")
+    }
+)
+.disposed(by: disposeBag)
+
+print("---- completable 2 -----")
+Completable.create { observer -> Disposable in
+    observer(.completed)
+    return Disposables.create()
+}
+.subscribe {
+    print($0)
+}
+.disposed(by: disposeBag)
