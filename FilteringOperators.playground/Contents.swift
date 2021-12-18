f77ecb39dc6f8313263f9ee094f65efe0b815a83
TDCIAN <tdcian71@gmail.com>
2021-12-18 23:51:12 +0900

    elementAt

--- FilteringOperators.playground/Contents.swift
+++ FilteringOperators.playground/Contents.swift
@@ -1,3 +1,37 @@
 import RxSwift
 
+let disposeBag = DisposeBag()
 print("--- ignoreElements")
+let sleepMode = PublishSubject<String>()
+
+sleepMode
+    .ignoreElements()
+    .subscribe {
+        print($0)
+    }
+    .disposed(by: disposeBag)
+
+sleepMode.onNext("😽")
+sleepMode.onNext("😽")
+sleepMode.onNext("😽")
+
+sleepMode.onCompleted()
+
+print("----- elementAt -----")
+let wakeUpWithTwoAlarm = PublishSubject<String>()
+
+wakeUpWithTwoAlarm
+    .element(at: 4)
+    .subscribe(onNext: {
+       print($0)
+    })
+    .disposed(by: disposeBag)
+
+wakeUpWithTwoAlarm
+    .onNext("🤣")
+wakeUpWithTwoAlarm
+    .onNext("🤣")
+wakeUpWithTwoAlarm
+    .onNext("🤪")
+wakeUpWithTwoAlarm
+    .onNext("😡")
