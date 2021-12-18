40d296acff9431389bb8bce2a59e00201c0487bb
TDCIAN <tdcian71@gmail.com>
2021-12-19 01:05:22 +0900

    filter

--- FilteringOperators.playground/Contents.swift
+++ FilteringOperators.playground/Contents.swift
@@ -35,3 +35,11 @@ wakeUpWithTwoAlarm
     .onNext("🤪")
 wakeUpWithTwoAlarm
     .onNext("😡")
+
+print("---- filter ----")
+Observable.of(1, 2, 3, 4, 5, 6, 7, 8) // [1, 2, 3, 4, 5, 6, 7, 8]
+    .filter { $0 % 2 == 0 }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
