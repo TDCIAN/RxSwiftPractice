7882450e74d6774ed1319f2a7644cafcb610d0ab
TDCIAN <tdcian71@gmail.com>
2021-12-24 08:51:58 +0900

    map, flatMap

--- TransformingOperators.playground/Contents.swift
+++ TransformingOperators.playground/Contents.swift
@@ -9,3 +9,18 @@ Observable.of("A", "B", "C")
         print($0)
     })
     .disposed(by: disposeBag)
+
+print("--- map ---")
+Observable.of(Date())
+    .map { date -> String in
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        return dateFormatter.string(from: date)
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("--- flatMap ---")
