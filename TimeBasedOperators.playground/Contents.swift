9f964624e688756c118c40d9ed01225c3c93ebe7
TDCIAN <tdcian71@gmail.com>
2022-01-03 08:26:25 +0900

    buffer

--- TimeBasedOperators.playground/Contents.swift
+++ TimeBasedOperators.playground/Contents.swift
@@ -36,4 +36,31 @@ print("--- buffer ---")
 let source = PublishSubject<String>()
 
 var count = 0
-let timer = 
+let timer = DispatchSource.makeTimerSource()
+
+timer.schedule(deadline: .now() + 2, repeating: .seconds(1))
+timer.setEventHandler {
+    count += 1
+    source.onNext("\(count)")
+}
+timer.resume()
+
+source
+    .buffer(
+        timeSpan: .seconds(2),
+        count: 2,
+        scheduler: MainScheduler.instance
+    )
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("--- window ---")
+let maxObervableCount = 1
+let timeToMake = RxTimeInterval.seconds(2)
+
+let window = PublishSubject<String>()
+
+let windowCount = 0
+
